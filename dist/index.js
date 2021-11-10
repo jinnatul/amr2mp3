60a0b747ea11ae5e8f84fb95b63617c56ca039a5
Morol <morolswediu@gmail.com>
2021-11-10 13:45:29 +0600

    init amr2mp3

--- dist/index.js
+++ dist/index.js
@@ -3,18 +3,43 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-var isPrime = exports.isPrime = function isPrime(number) {
-  var primeArr = new Array(number + 1).fill(true);
-  primeArr[0] = false;
-  primeArr[1] = false;
-  for (var num = 2; num <= number; num += 1) {
-    if (primeArr[num] === true) {
-      var nextnum = num * num;
-      while (nextnum <= number) {
-        primeArr[nextnum] = false;
-        nextnum += num;
-      }
+exports.amr2mp3 = undefined;
+
+var _child_process = require("child_process");
+
+var _path = require("path");
+
+var _path2 = _interopRequireDefault(_path);
+
+var _ffmpegStatic = require("ffmpeg-static");
+
+var _ffmpegStatic2 = _interopRequireDefault(_ffmpegStatic);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var amr2mp3 = exports.amr2mp3 = function amr2mp3(filepath) {
+  var outputDir = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : "./src/mp3";
+  var outputName = arguments[2];
+
+  return new Promise(function (resolve, reject) {
+    var _path$parse = _path2.default.parse(filepath),
+        ext = _path$parse.ext,
+        filename = _path$parse.name;
+
+    if (ext.toLocaleLowerCase() != ".amr") {
+      console.log(filepath + " is not a .amr file");
+      reject(new Error(filepath + " is not a .amr file"));
+      return;
     }
-  }
-  return primeArr[number];
+    var _outputName = outputName || filename;
+    var cmdStr = _ffmpegStatic2.default + " -y -i \"" + _path2.default.normalize(filepath) + "\" -acodec libmp3lame -ar 24000 -vol 500 -ab 128 \"" + _path2.default.join(outputDir, _outputName + ".mp3") + "\"";
+    console.log(cmdStr);
+    (0, _child_process.exec)(cmdStr, function (err, stdout, stderr) {
+      if (err) {
+        reject(new Error("error:" + stderr));
+      } else {
+        resolve(outputDir + "/" + _outputName + ".mp3");
+      }
+    });
+  });
 };
--- src/index.js
+++ src/index.js
@@ -1,15 +1,29 @@
-export const isPrime = (number) => {
-  let primeArr = new Array(number + 1).fill(true);
-  primeArr[0] = false;
-  primeArr[1] = false;
-  for (let num = 2; num <= number; num += 1) {
-    if (primeArr[num] === true) {
-      let nextnum = num * num;
-      while (nextnum <= number) {
-        primeArr[nextnum] = false;
-        nextnum += num;
-      }
+import { exec } from "child_process";
+import path from "path";
+import ffmpegPath from "ffmpeg-static";
+
+export const amr2mp3 = (filepath, outputDir = "./src/mp3", outputName) => {
+  return new Promise((resolve, reject) => {
+    const { ext, name: filename } = path.parse(filepath);
+    if (ext.toLocaleLowerCase() != ".amr") {
+      console.log(`${filepath} is not a .amr file`);
+      reject(new Error(`${filepath} is not a .amr file`));
+      return;
     }
-  }
-  return primeArr[number];
+    const _outputName = outputName || filename;
+    const cmdStr = `${ffmpegPath} -y -i "${path.normalize(
+      filepath
+    )}" -acodec libmp3lame -ar 24000 -vol 500 -ab 128 "${path.join(
+      outputDir,
+      _outputName + ".mp3"
+    )}"`;
+    console.log(cmdStr);
+    exec(cmdStr, (err, stdout, stderr) => {
+      if (err) {
+        reject(new Error("error:" + stderr));
+      } else {
+        resolve(`${outputDir}/${_outputName}.mp3`);
+      }
+    });
+  });
 };
